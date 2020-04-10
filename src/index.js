9e4dbfc9c492c6cf6a4d247774b6141aa19f5fca
sivasankar <guntupallisivasankar@gmail.com>
2020-04-10 11:29:17 +0530

    feat:thunk (#3)
    Co-authored-by: sivasankar9 <guntuapllisivasankar@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { render } from 'react-dom';
 import './style.css';
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import App from './App';
 import {Provider} from 'react-redux';
-import {createStore, combineReducers} from 'redux'
+import React from 'react';
 import calenderEvents from './components/full-calender/reducer';
+import { render } from 'react-dom';
+import thunkMiddleware from 'redux-thunk';
+ 
 
 let store = createStore(combineReducers({
     fullCalender:calenderEvents,
+}),compose(
+      applyMiddleware(thunkMiddleware),
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    ));
 
-}))
 
 render(<Provider store = {store}>
     <App />

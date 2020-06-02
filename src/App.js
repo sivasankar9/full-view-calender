fee798719e182e9240013ce3bd722951f536355f
sivasankar <guntupallisivasankar@gmail.com>
2020-06-02 19:37:33 +0530

    feat:faiul (#53)
    Co-authored-by: sivasankar9 <>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React,{Component} from 'react';
+import PropTypes from 'prop-types';
+
+const upper = (a)=>{a.toUpperCase()};
 class App extends Component{
 
     render(){
-        return (<div> Welcome siva sankar, manasa & Roith.
+        return (<div>namez:::{upper([])} Welcome siva sankar, manasa & Roith.
             </div>);
     }
 }
+
+App.propTypes = {
+  namez: PropTypes.string.isRequired
+}
+
 export default App;

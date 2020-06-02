5bfdc90a143cbb17407d9fe1f99049865f729b06
sivasankar9 <>
2020-06-02 12:44:39 +0530

    updated greeting

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 class App extends Component{
 
     render(){
-        return (<div> Welcome siva sankar
+        return (<div> Welcome siva sankar & manasa
             </div>);
     }
 }

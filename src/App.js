ba95e28d6795e08be21f0702696e4f014eb045cb
sivasankar9 <>
2020-06-02 17:59:42 +0530

    feat:build fauilur

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 class App extends Component{
 
-    render(){
+    rendert(){
         return (<div> Welcome siva sankar & manasa
             </div>);
     }

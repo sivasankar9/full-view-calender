acc54dfcec869a0aa8b28218c2548de4ff1b0672
sivasankar9 <>
2020-06-02 09:59:15 +0530

    removed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React,{Component} from 'react';
-import EventCalender from './components/full-calender';
-import Loader from './components/full-calender/components/shared/loader';
 class App extends Component{
 
     render(){
-        return (<div>
-            <Loader />
-
-            <EventCalender />
+        return (<div> Welcome
             </div>);
     }
 }
